b7bc265aedcf03b3a2b5ae6ec3afa4260840447c
AnuchitO <anuchit.prasertasng@gmail.com>
2020-03-14 11:41:55 +0700

    change encrypt to interface

--- main.go
+++ main.go
@@ -13,7 +13,11 @@ type User struct {
 	Password string `encrypt:"base64"`
 }
 
-func encryptData(user *User) {
+type Secret struct {
+	Token string `encrypt:"base64"`
+}
+
+func encryptData(user interface{}) {
 	t := reflect.TypeOf(user).Elem()
 	for i := 0; i < t.NumField(); i++ {
 		field := t.Field(i)
@@ -36,4 +40,11 @@ func main() {
 	encryptData(&user)
 
 	fmt.Println(user)
+
+	secret := Secret{Token: "token1234"}
+	fmt.Println(secret)
+
+	encryptData(&secret)
+
+	fmt.Println(secret)
 }
